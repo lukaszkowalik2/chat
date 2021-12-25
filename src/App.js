172494c4343e914ac1e20b00e5ef26889657c4ca
unknown <lkowalik04@gmail.com>
2021-12-25 15:56:45 +0100

    struktura chatu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './styles/main.scss'
+import LeftSideBar from './components/leftSideBar';
+import Chat from './components/chat'
+import Details from './components/details';
+import Header from './components/header';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="app">
+      <Header/>
+    <div class="wrapper">
+      <LeftSideBar/>
+      <Chat/>
+      <Details/>
     </div>
-  );
+  </div>
+);
 }
-
+ 
 export default App;

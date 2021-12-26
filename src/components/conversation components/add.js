7caea43de65ef5bdd562348e0a492f8bfde60699
unknown <lkowalik04@gmail.com>
2021-12-26 15:40:31 +0100

    robienie nowych kolekcji

---
+++ src/components/conversation components/add.js
@@ -0,0 +1,7 @@
+// import { doc, setDoc } from "firebase/firestore"; 
+// import db from '../../firebase'
+// await setDoc(doc(db, "cities", "LA"), {
+//   name: "Los Angeles",
+//   state: "CA",
+//   country: "USA"
+// });
